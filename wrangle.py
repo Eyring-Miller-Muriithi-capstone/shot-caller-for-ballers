c1516825273f8172d7c98dd13984b454fe4b54ce
Ryan Miller <ryan.d.miller05@gmail.com>
2022-08-10 09:28:07 -0500

    moved encode function

--- wrangle.py
+++ wrangle.py
@@ -133,18 +133,22 @@ def create_game_event(df):
 
     return df, df_outlier_3pt
 
-def encoder(df):
+def encoder(train, validate, test):
     '''
     '''
     # Encode target
-    df['shot_made_flag'] = np.where(df.shot_result == 'Made Shot',1,0)
+    train['shot_made_flag'] = np.where(train.shot_result == 'Made Shot',1,0)
+    validate['shot_made_flag'] = np.where(validate.shot_result == 'Made Shot',1,0)
+    test['shot_made_flag'] = np.where(test.shot_result == 'Made Shot',1,0)
 
     # Encode these columns
     encode_cols = ['home','zone','shot_type']
 
-    df_encoded = pd.get_dummies(df, columns = encode_cols)
+    train_encoded = pd.get_dummies(train, columns = encode_cols)
+    validate_encoded = pd.get_dummies(validate, columns = encode_cols)
+    test_encoded = pd.get_dummies(test, columns = encode_cols)
 
-    return df_encoded
+    return train_encoded, validate_encoded, test_encoded
 
 def pre_wrangle_prep():
     df = tome_prep()
@@ -153,9 +157,9 @@ def pre_wrangle_prep():
     df = create_metrics(df)
     df = create_location(df)
     df , df_outlier_3pt = create_game_event(df)
-    df = encoder(df)
     train, validate, test = splitter(df, target = 'shot_result')
     X_train_exp = train
+    train, validate, test = encoder(train, validate,test)
     train_scaled, validate_scaled, test_scaled = scaling_minmax(train, validate, test)
     X_train, y_train, X_validate, y_validate, X_test, y_test = seperate_X_y(train_scaled, validate_scaled, test_scaled) 
 
@@ -216,15 +220,6 @@ def seperate_X_y(train_scaled, validate_scaled, test_scaled):
 
     return X_train, y_train, X_validate, y_validate, X_test, y_test
 
-# ------------MODELING BELOW -------------
-#baseline accuracy
-BASELINE_ACCURACY = 0.6424
-
-#taxicab number
-RAND_SEED = 1802
-
-### FLOW CONTROL FUNCTIONS
-
 def splitter(df, target = 'None', train_split_1 = .8, train_split_2 = .7, random_state = 123):
     '''
     Splits a dataset into train, validate and test dataframes.
@@ -241,263 +236,4 @@ def splitter(df, target = 'None', train_split_1 = .8, train_split_2 = .7, random
         train, test = train_test_split(df, train_size = train_split_1, random_state = random_state, stratify = df[target])
         train, validate = train_test_split(train, train_size = train_split_2, random_state = random_state, stratify = train[target])
         print(f'Train = {train.shape[0]} rows ({100*(train_split_1*train_split_2):.1f}%) | Validate = {validate.shape[0]} rows ({100*(train_split_1*(1-train_split_2)):.1f}%) | Test = {test.shape[0]} rows ({100*(1-train_split_1):.1f}%)')
-        return train, validate, test  
-
-def model_maker(X_train, y_train, X_validate, y_validate, baseline_acc = BASELINE_ACCURACY):
-    """
-    Makes a mass of models and returns a dataframe with accuracy metric
-    """
-    outputs = []
-    #make logistic regression model
-    outputs.append(make_log_reg_model(X_train, y_train, X_validate, y_validate, baseline_acc))
-    #make knn and decision trees
-    for i in range(1, 25):
-        outputs.append(make_knn_model(X_train, y_train, X_validate, y_validate, i, baseline_acc))
-        outputs.append(make_decision_tree_model(X_train, y_train, X_validate, y_validate, i, baseline_acc))
-    rand_forest_params = return_product(3, 5, 4)
-    print('starting rf and et')
-    #make random forest and extra tree models
-    for prod in rand_forest_params:
-        #print('making rf and et')
-        outputs.append(make_random_forest_model(X_train, y_train, X_validate, y_validate, leaf=prod[0], depth=prod[1], trees = prod[2], baseline_acc = baseline_acc))
-        outputs.append(make_extra_trees_model(X_train, y_train, X_validate, y_validate, leaf=prod[0], depth=prod[1], trees = prod[2], baseline_acc = baseline_acc))
-    print('finished rf and et')
-    estimators = [
-        {'model':LogisticRegression(), 'name':'LogisticRegression'},
-        {'model':KNeighborsClassifier(), 'name':'KNeighborsClassifier'},
-        {'model':DecisionTreeClassifier(), 'name':'DecisionTreeClassifier'},
-        {'model':ExtraTreesClassifier(), 'name':'ExtraTreesClassifier'}
-    ]
-    #make ensemble model
-    for estimator in estimators:
-        outputs.append(make_bagging_classifier(X_train, y_train, X_validate, y_validate, estimator['model'], estimator['name'], baseline_acc = baseline_acc))
-    return pd.DataFrame(outputs)
-
-def baseline_model_maker(y_train, y_validate):
-    """
-    Creates a baseline model and returns metrics on train and validate sets
-    """
-    #get the relevant columns
-    baseline_model = pd.DataFrame(y_train)
-    baseline_model_val = pd.DataFrame(y_validate)
-    #prediction is the mode of the langauges
-    baseline_model['predicted'] = y_train.mode().to_list()[0]
-    baseline_model_val['predicted'] = y_train.mode().to_list()[0]
-    #get the metrics
-    metrics_dict = metrics.classification_report(y_train, baseline_model['predicted'], output_dict=True, zero_division = True)
-    metrics_dict_val = metrics.classification_report(y_validate, baseline_model_val['predicted'], output_dict=True, zero_division = True)
-    output = {
-        'model':'Baseline Model',
-        'train_accuracy': metrics_dict['accuracy'],
-        'validate_accuracy': metrics_dict_val['accuracy'],
-    }
-    #return the metrics as a dataframe and the baseline accuracy for the model
-    return pd.DataFrame([output]), metrics_dict['accuracy']
-
-
-def test_model(X_train, y_train, X_validate, y_validate, X_test, y_test, baseline_acc = BASELINE_ACCURACY):
-    """
-    Final model trained, and then run on unseen test data.  Dataframe of metrics returned
-    """
-    #make and train the model
-    bc = BaggingClassifier(base_estimator=DecisionTreeClassifier(), n_estimators = 20, max_samples = 0.5, max_features = 0.5, bootstrap=False, random_state = RAND_SEED).fit(X_train, y_train['language'])
-    
-    # Turn y Series into dataframes
-    y_train = pd.DataFrame(y_train)
-    y_validate = pd.DataFrame(y_validate)
-    y_test = pd.DataFrame(y_test)
-    
-    #get predictions
-    y_train['predicted'] = bc.predict(X_train)
-    y_validate['predicted'] = bc.predict(X_validate)
-    y_test['predicted'] = bc.predict(X_test)
-    #get the metrics
-    metrics_dict = metrics.classification_report(y_train['shot_result'], y_train['predicted'], output_dict=True, zero_division=True)
-    metrics_dict_val = metrics.classification_report(y_validate['shot_result'], y_validate['predicted'], output_dict=True, zero_division=True)
-    metrics_dict_test = metrics.classification_report(y_test['shot_result'], y_test['predicted'], output_dict=True, zero_division=True)
-    output = {
-        'model':'BaggingClassifier',
-        'attributes':f'estimator = DecisionTreeClassifier',
-        'train_accuracy': metrics_dict['accuracy'],
-        'validate_accuracy': metrics_dict_val['accuracy'],
-        'test_accuracy':metrics_dict_test['accuracy'],
-        'better_than_baseline':metrics_dict_test['accuracy'] > baseline_acc
-    }
-    #return the dataframe
-    return pd.DataFrame([output])
-
-### UTILITY FUNCTIONS
-
-# SPLITTER
-
-def return_product(l, d, t):
-    """
-    makes a itertools object iterable for the random forest and extra trees models
-    """
-    #make the range sets
-    leaf_vals = range(1,l)
-    depth_vals = range(2,d)
-    #make tree values starting at 100 and going up in steps of 50
-    tree_values = range(100, t*100, 50)
-    #make the cartesian product
-    product_output = product(leaf_vals, depth_vals, tree_values)
-    return product_output
-
-
-### MODEL MAKERS
-
-def make_bagging_classifier(X_train, y_train, X_validate, y_validate, estimator, estimator_name, baseline_acc = BASELINE_ACCURACY):
-    """
-    Makes a bagging classifier based on passed estimator
-    """
-    # Turn y Series into dataframes
-    y_train = pd.DataFrame(y_train)
-    y_validate = pd.DataFrame(y_validate)
-
-    #fit model
-    bc = BaggingClassifier(base_estimator=estimator, n_estimators = 20, max_samples = 0.5, max_features = 0.5, bootstrap=False, random_state = RAND_SEED).fit(X_train, y_train['language'])
-    #predict
-    y_train['predicted'] = bc.predict(X_train)
-    y_validate['predicted'] = bc.predict(X_validate)
-    #get metrics
-    metrics_dict = metrics.classification_report(y_train['shot_result'], y_train['predicted'], output_dict=True, zero_division=True)
-    metrics_dict_val = metrics.classification_report(y_validate['shot_result'], y_validate['predicted'], output_dict=True, zero_division=True)
-    output = {
-        'model':'BaggingClassifier',
-        'attributes':f'estimator = {estimator_name}',
-        'train_accuracy': metrics_dict['accuracy'],
-        'validate_accuracy': metrics_dict_val['accuracy'],
-        'better_than_baseline':metrics_dict['accuracy'] > baseline_acc and metrics_dict_val['accuracy'] > baseline_acc
-    }
-    return output
-
-def make_log_reg_model(X_train, y_train, X_validate, y_validate, baseline_acc = BASELINE_ACCURACY):
-    """
-    makes a logistic regression model
-    """
-    # Turn y Series into dataframes
-    y_train = pd.DataFrame(y_train)
-    y_validate = pd.DataFrame(y_validate)
-
-    #REMOVE BELOW----X
-    X_train = X_train.drop(columns = ['zone','shot_type'])
-    X_validate = X_validate.drop(columns = ['zone','shot_type'])
-    #REMOVE ABOVE----X
-
-    #fit the model
-    lm = LogisticRegression().fit(X_train, y_train['shot_result'])
-    #make predictions
-    y_train['predicted'] = lm.predict(X_train)
-    y_validate['predicted'] = lm.predict(X_validate)
-    # get metrics
-    metrics_dict = metrics.classification_report(y_train['shot_result'], y_train['predicted'], output_dict=True, zero_division=True)
-    metrics_dict_val = metrics.classification_report(y_validate['shot_result'], y_validate['predicted'], output_dict=True, zero_division=True)
-    output = {
-        'model':'LogisticRegression',
-        'attributes':'None',
-        'train_accuracy': metrics_dict['accuracy'],
-        'validate_accuracy': metrics_dict_val['accuracy'],
-        'better_than_baseline':metrics_dict['accuracy'] > baseline_acc and metrics_dict_val['accuracy'] > baseline_acc
-    }
-    return output
-
-def make_knn_model(X_train, y_train, X_validate, y_validate, neighbors, baseline_acc = BASELINE_ACCURACY):
-    """
-    Make a knn model
-    """
-    # Turn y Series into dataframes
-    y_train = pd.DataFrame(y_train)
-    y_validate = pd.DataFrame(y_validate)
-    #fit the model
-    knn = KNeighborsClassifier(n_neighbors = neighbors)
-    knn = knn.fit(X_train, y_train['shot_result'])
-    #make predictions
-    y_train['predicted'] = knn.predict(X_train)
-    y_validate['predicted'] = knn.predict(X_validate)
-    #get metrics
-    metrics_dict = metrics.classification_report(y_train['shot_result'], y_train['predicted'], output_dict=True, zero_division=True)
-    metrics_dict_val = metrics.classification_report(y_validate['shot_result'], y_validate['predicted'], output_dict=True, zero_division=True)
-    output = {
-        'model':'KNNeighbors',
-        'attributes':f'n_neighbors = {neighbors}',
-        'train_accuracy': metrics_dict['accuracy'],
-        'validate_accuracy': metrics_dict_val['accuracy'],
-        'better_than_baseline':metrics_dict['accuracy'] > baseline_acc and metrics_dict_val['accuracy'] > baseline_acc
-    }
-    return output
-
-def make_decision_tree_model(X_train, y_train, X_validate, y_validate, depth, baseline_acc = BASELINE_ACCURACY):
-    """
-    Makes a decision tree model and returns a dictionary containing calculated accuracy metrics
-    """
-    # Turn y Series into dataframes
-    y_train = pd.DataFrame(y_train)
-    y_validate = pd.DataFrame(y_validate)
-
-    #make and fit the model
-    dt = DecisionTreeClassifier(max_depth = depth, random_state=RAND_SEED)
-    dt = dt.fit(X_train, y_train['shot_result'])
-    #make predictions
-    y_train['predicted'] = dt.predict(X_train)
-    y_validate['predicted'] = dt.predict(X_validate)
-    # calculate metrics
-    metrics_dict = metrics.classification_report(y_train['shot_result'], y_train['predicted'], output_dict=True, zero_division=True)
-    metrics_dict_val = metrics.classification_report(y_validate['shot_result'], y_validate['predicted'], output_dict=True, zero_division=True)
-    output = {
-        'model':'Decision Tree Classifier',
-        'attributes': f"max_depth = {depth}",
-        'train_accuracy': metrics_dict['accuracy'],
-        'validate_accuracy': metrics_dict_val['accuracy'],
-        'better_than_baseline':metrics_dict['accuracy'] > baseline_acc and metrics_dict_val['accuracy'] > baseline_acc
-    }
-    return output
-
-def make_random_forest_model(X_train, y_train, X_validate, y_validate, leaf, depth, trees, baseline_acc = BASELINE_ACCURACY):
-    """
-    Makes a random forest model
-    """
-    # Turn y Series into dataframes
-    y_train = pd.DataFrame(y_train)
-    y_validate = pd.DataFrame(y_validate)
-    #make and fit the model
-    rf = RandomForestClassifier(min_samples_leaf = leaf, max_depth=depth, n_estimators=trees, random_state=RAND_SEED)
-    rf = rf.fit(X_train, y_train['shot_result'])
-    #make predictions
-    y_train['predicted'] = rf.predict(X_train)
-    y_validate['predicted'] = rf.predict(X_validate)
-    # calculate metrics
-    metrics_dict = metrics.classification_report(y_train['shot_result'], y_train['predicted'], output_dict=True, zero_division=True)
-    metrics_dict_val = metrics.classification_report(y_validate['shot_result'], y_validate['predicted'], output_dict=True, zero_division=True)
-    output = {
-        'model':'Random Forest Classifier',
-        'attributes': f"leafs = {leaf} : depth = {depth} : trees = {trees}",
-        'train_accuracy': metrics_dict['accuracy'],
-        'validate_accuracy': metrics_dict_val['accuracy'],
-        'better_than_baseline':metrics_dict['accuracy'] > baseline_acc and metrics_dict_val['accuracy'] > baseline_acc
-    }
-    return output
-
-def make_extra_trees_model(X_train, y_train, X_validate, y_validate, leaf, depth, trees, baseline_acc = BASELINE_ACCURACY):
-    """
-    makes an extra trees model
-    """
-    # Turn y Series into dataframes
-    y_train = pd.DataFrame(y_train)
-    y_validate = pd.DataFrame(y_validate)
-    #make and fit the model
-    et = ExtraTreesClassifier(min_samples_leaf = leaf, max_depth=depth, n_estimators=trees, random_state=RAND_SEED)
-    et = et.fit(X_train, y_train['shot_result'])
-    #make predictions
-    y_train['predicted'] = et.predict(X_train)
-    y_validate['predicted'] = et.predict(X_validate)
-    # calculate metrics
-    metrics_dict = metrics.classification_report(y_train['shot_result'], y_train['predicted'], output_dict=True, zero_division=True)
-    metrics_dict_val = metrics.classification_report(y_validate['shot_result'], y_validate['predicted'], output_dict=True, zero_division=True)
-    output = {
-        'model':'Extra Trees Classifier',
-        'attributes': f"leafs = {leaf} : depth = {depth} : trees = {trees}",
-        'train_accuracy': metrics_dict['accuracy'],
-        'validate_accuracy': metrics_dict_val['accuracy'],
-        'better_than_baseline':metrics_dict['accuracy'] > baseline_acc and metrics_dict_val['accuracy'] > baseline_acc
-    }
-    return output
+        return train, validate, test
